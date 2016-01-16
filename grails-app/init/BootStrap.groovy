01da36ccb310c5bc6a34b3ea237463e5b7e2a811
zyro <zyro@zyro.net>
2016-01-16 08:18:58 +0100

    cleanup to reduce sample

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -3,8 +3,6 @@ import javax.money.Monetary
 class BootStrap {
 
     def init = { servletContext ->
-		println System.properties.foo
-		println System.properties.springloaded
 		Monetary.getCurrency("USD");
     }
     def destroy = {
